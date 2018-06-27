43c8661f080598371103b4df2b2697afc4d2f0a9
Julian McNeill <julesdowork@gmail.com>
2018-06-26 23:45:07 -0400

    Add CounterButton2 import to Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import CounterButton from "./CounterButton";
+import CounterButton2 from "./CounterButton2";
 
 class Header extends Component {
   // shouldComponentUpdate(nextProps, nextState) {
